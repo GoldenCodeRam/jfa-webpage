d082e05eead53e2870c11e3647080a213be7a86c
Luis Alejandro Quiroga Gómez <lualquigo@gmail.com>
2023-10-17 09:25:41 -0500

    feat: Collapsible elements, images and updates dependencies

---
+++ resources/js/Components/Collapse.svelte
@@ -0,0 +1,18 @@
+<script>
+    import {slide} from "svelte/transition";
+
+    let showCollapse = false;
+
+    function toggleCollapse() {
+        showCollapse = !showCollapse;
+    }
+</script>
+
+<button type="button" on:click={toggleCollapse} class="w-full">
+    <slot name="button" />
+</button>
+{#if showCollapse}
+<div transition:slide>
+    <slot name="content"/>
+</div>
+{/if}
--- resources/js/Components/Sidebar.svelte
+++ resources/js/Components/Sidebar.svelte
@@ -1,5 +1,6 @@
 <script>
-    import { blur, slide } from "svelte/transition";
+    import { blur } from "svelte/transition";
+    import Collapse from "./Collapse.svelte";
 
     let isSidebarOpen = false;
 
@@ -24,14 +25,16 @@
 {#if isSidebarOpen}
     <div role="dialog" aria-modal="true">
         <!-- Background backdrop, show/hide based on slide-over state. -->
-        <div class="fixed inset-0 z-10 bg-black/10 backdrop-blur-sm" transition:blur></div>
         <div
-            transition:slide={{ axis: "x" }}
+            class="fixed inset-0 z-10 bg-black/10 backdrop-blur-sm"
+            transition:blur
+        ></div>
+        <div
+            transition:blur
             class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div class="flex items-center justify-between">
-                <a href="#" class="-m-1.5 p-1.5">
-                    <span class="sr-only">Your Company</span>
-                    <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="">
+                <a href="#" class="-m-1.5 p-1.5 font-extrabold text-sky-800">
+                    JFA Consultorías SAS
                 </a>
                 <button on:click={closeSidebar} type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700">
                     <span class="sr-only">Close menu</span>
@@ -47,35 +50,34 @@
                             ¿Quiénes somos?
                         </a>
                         <div class="-mx-3">
-                            <button type="button" class="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50" aria-controls="disclosure-1" aria-expanded="false">
-                                Nuestro equipo
-                                <svg class="h-5 w-5 flex-none" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                    <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
-                                </svg>
-                            </button>
-                            <!-- 'Product' sub-menu, show/hide based on menu state. -->
-                            <div class="mt-2 space-y-2" id="disclosure-1">
-                                <a href="#" class="flex gap-3 rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50">
-                                    <img class="w-12 h-auto" src="/img/logo.svg" />
-                                    JFA Consultorias
-                                </a>
-                                <a href="#" class="flex gap-3 rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50">
-                                    <img class="w-12 h-auto" src="/img/logo.svg" />
-                                    JFA Ambiental
-                                </a>
-                                <a href="#" class="flex gap-3 rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50">
-                                    <img class="w-12 h-auto" src="/img/logo.svg" />
-                                    JFA Seguridad y Salud
-                                </a>
-                                <a href="#" class="flex gap-3 rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50">
-                                    <img class="w-12 h-auto" src="/img/logo.svg" />
-                                    JFA Administrativo
-                                </a>
-                                <a href="#" class="flex gap-3 rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50">
-                                    <img class="w-12 h-auto" src="/img/logo.svg" />
-                                    JFA Legal
-                                </a>
-                            </div>
+                            <Collapse>
+                                <span slot="button" class="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+                                    Nuestro equipo
+                                    <svg class="h-5 w-5 flex-none" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                        <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
+                                    </svg>
+                                </span>
+                                <span slot="content">
+                                    <div class="mt-2 space-y-2" id="disclosure-1">
+                                        <a href="#" class="flex gap-3 rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+                                            <img class="w-12 h-auto" src="/img/logo.svg" />
+                                            JFA Consultorías
+                                        </a>
+                                        <a href="#" class="flex gap-3 rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+                                            <img class="w-12 h-auto" src="/img/logo_ambiental.svg" />
+                                            JFA Ambiental
+                                        </a>
+                                        <a href="#" class="flex gap-3 rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+                                            <img class="w-12 h-auto" src="/img/logo_sys.svg" />
+                                            JFA Seguridad y Salud
+                                        </a>
+                                        <a href="#" class="flex gap-3 rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+                                            <img class="w-12 h-auto" src="/img/logo_legal.svg" />
+                                            JFA Legal
+                                        </a>
+                                    </div>
+                                </span>
+                            </Collapse>
                         </div>
                         <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
                             Contáctanos
